5de62a68d99d4d9894412c5d4e0be20123e3356e
Oka R. Abdillah <hans.abdillh05@gmail.com>
2021-12-23 16:59:05 +0800

    Create handler for insert new candidate

--- src/views/NewCandidate.vue
+++ src/views/NewCandidate.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, reactive, computed } from 'vue'
-import { mdiAccountGroup, mdiContentSaveMoveOutline } from '@mdi/js'
+import { mdiAccountGroup, mdiContentSaveMoveOutline, mdiInformation } from '@mdi/js'
 import MainSection from '@/components/MainSection.vue'
 import Notification from '@/components/Notification.vue'
 import CardComponent from '@/components/CardComponent.vue'
@@ -11,31 +11,58 @@ import Field from '@/components/Field.vue'
 import Control from '@/components/Control.vue'
 import JbButtons from '@/components/JbButtons.vue'
 import JbButton from '@/components/JbButton.vue'
+import http from '@/helper/http.js'
 
 const titleStack = ref(['Admin', 'Tambah Paslon'])
 
-
 //Form handler
 const form = reactive({
-	chairmanName: '',
-	chairmanImage: '',
-	viceChairmanName: '',
-	viceChairmanImage: '',
-	candidateNumber: ''	
+	chairman_name: '',
+	chairman_image: '',
+	vice_chairman_name: '',
+	vice_chairman_image: '',
+	candidate_number: ''	
 })
 
+//Notification handler
+const showNotif = ref(false)
+const textNotif = ref('')
+const colorNotif = ref()
+
+//Insert handler
+const submit = () => {
+   http.post('master/insert-candidate', form, (data, status) => {
+      if (status) {
+         showNotif.value = true
+         textNotif.value = data.response
+         colorNotif.value = 'info'
+      }
+      else { 
+         textNotif.value = data.sqlMessage
+         showNotif.value = true
+         colorNotif.value = 'warning'
+      }
+   })
+}
 </script>
 
 <template>
   <title-bar :title-stack="titleStack" />
   <hero-bar>Tambah Pasangan Calon Baru</hero-bar>
   <main-section>
+     <Notification 
+      v-on:close="showNotif = false"
+      v-if="showNotif"
+      :color="colorNotif"
+      :icon="mdiInformation">
+        {{ textNotif }}
+   </Notification>
   <card-component
       class="mb-6"
     >
     <Field label="Nomor urut pasangan">
        <Control
-         v-model="form.candidateNumber"
+         v-model="form.candidate_number"
          class="mb-6"
          type="number"
          placeholder="Masukkan nomor urut pasangan calon" />
@@ -43,28 +70,28 @@ const form = reactive({
           
       <Field label="Calon Ketua Umum">
         <Control
-          v-model="form.chairmanName"
+          v-model="form.chairman_name"
           class="mb-6"
           placeholder="Nama lengkap calon" />
       </Field>
 
       <Field label="Foto Calon Ketua Umum">
         <Control
-          v-model="form.chairmanImage"
+          v-model="form.chairman_image"
           class="mb-6"
           placeholder="Link upload foto calon" />
       </Field>
 
       <Field label="Calon Wakil Ketua Umum">
         <Control
-          v-model="form.viceChairmanName"
+          v-model="form.vice_chairman_name"
           class="mb-6"
           placeholder="Nama lengkap calon" />
       </Field>
 
       <Field label="Foto Calon Wakil Ketua Umum">
         <Control
-          v-model="form.viceChairmanImage"
+          v-model="form.vice_chairman_image"
           class="mb-6"
           placeholder="Link upload foto calon" />
       </Field>
@@ -73,6 +100,7 @@ const form = reactive({
        <JbButtons type="justify-start lg:justify-end">
          <JbButton 
            label="Submit"
+           @click="submit()"
            :icon="mdiContentSaveMoveOutline"
            color="info" />
        </JbButtons>
